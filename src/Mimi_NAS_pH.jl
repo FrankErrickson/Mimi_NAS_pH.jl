6fd05d8a9fde886cc5b7d659df11237f73aaa049
Frank C. Errickson <fce@princeton.edu>
2021-08-24 15:09:52 -0400

    Update main file to work with IPCC consistent example CO2 data

--- src/Mimi_NAS_pH.jl
+++ src/Mimi_NAS_pH.jl
@@ -8,17 +8,18 @@ using CSVFiles, DataFrames, Mimi
 include(joinpath("components", "ocean_ph_component.jl"))
 
 # Load example CO₂ concentration data to run the Ocean pH function.
-co2_data = DataFrame(load(joinpath(@__DIR__, "..", "data", "ssp_co2_concentrations.csv"), skiplines_begin=6))
+co2_data = DataFrame(load(joinpath(@__DIR__, "..", "data", "ssp_co2_concentrations.csv"), skiplines_begin=4))
 
 
 # Create a function to set up and run the Ocean pH compoennt as a stand-alone Mimi model.
-function get_model()
+# 'ssp_emissions_scenario' options = "SSP1-19", "SSP1-26", "SSP2-45", "SSP3-70", "SSP5-85"
+function get_model(;ssp_emissions_scenario::String="SSP1-19")
 
     # Initialize a Mimi model.
     m = Model()
 
     # Set time dimension.
-    set_dimension!(m, :time, collect(2005:2100))
+    set_dimension!(m, :time, collect(1950:2100))
 
     # Add in ocean pH Mimi component.
     add_comp!(m, ocean_pH)
@@ -26,8 +27,8 @@ function get_model()
     # Set ocean pH parameters.
     set_param!(m, :ocean_pH, :β1, -0.3671)
     set_param!(m, :ocean_pH, :β2, 10.2328)
-    set_param!(m, :ocean_pH, :pH_0, 8.0524)
-    set_param!(m, :ocean_pH, :atm_co2_conc, co2_data[:, "SSP1-Baseline"])
+    set_param!(m, :ocean_pH, :pH_0, 8.123)
+    set_param!(m, :ocean_pH, :atm_co2_conc, co2_data[:, ssp_emissions_scenario])
 
     # Return ocean pH model.
     return m
